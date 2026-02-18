a57ebc53d3e87155119e46d842543c5cae7a30f8
sanjaycoder01 <sjkulkarni31@gmail.com>
2026-02-18 16:01:42 +0530

    redeploy

--- src/config/index.js
+++ src/config/index.js
@@ -10,4 +10,5 @@ module.exports = {
     jwtRefreshSecret: process.env.JWT_REFRESH_SECRET || process.env.JWT_SECRET,
     jwtExpiresIn: process.env.JWT_EXPIRES_IN || '20h',
     jwtRefreshExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN || '7d'
+
 };
